c79803edd559109d01ce7b9cc3c9db6912dcc52d
marsdenmadoka <marsmadoka98@gmail.com>
2020-08-04 10:29:02 +0300

    added a new firebase project

---
+++ reactfirebase/src/components/projects/ProjectDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectDetails=()=> {
+    return (
+        <div className="container section project-details">
+          <div className="card z-depth-0">
+              <div className="card-content">
+                  <span className="card-title">Project title</span>
+                  <p>Pariatur cupidatat quis ad pariatur proident anim eu sunt consequat laborum ad et cupidatat. Lorem veniam voluptate elit aliquip voluptate aliquip fugiat esse sunt. Commodo dolor culpa anim in aliqua enim. Laboris ad consectetur commodo voluptate excepteur velit qui voluptate non laboris commodo. Minim laborum nisi eu amet laboris anim aliqua.</p>
+              <div className="card-action gret lighten-4 grey-text"></div>
+              <div>posted by madoka</div>
+              <div>date</div>
+              </div>
+            </div>  
+            </div>
+    )
+}
+
+export default ProjectDetails
