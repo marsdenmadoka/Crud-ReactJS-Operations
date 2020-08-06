96128c12eb1534879daebce0a0b800f65bb1a4ff
marsdenmadoka <marsmadoka98@gmail.com>
2020-08-06 23:49:42 +0300

    added nw files

--- reactfirebase/src/components/projects/ProjectDetails.js
+++ reactfirebase/src/components/projects/ProjectDetails.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import {Redirect} from  'react-router-dom'
-
+import moment from 'moment'
 
 const ProjectDetails=(props)=> {
     const { project,auth } = props;
@@ -18,7 +18,7 @@ const ProjectDetails=(props)=> {
        <p>{project.content}</p>
             <div className="card-action gret lighten-4 grey-text"></div>
     <div>posted by {project.authorFirstName} {project.authorLastName}</div>
-            <div>date</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
             </div>
           </div>  
           </div>
--- reactfirebase/src/components/projects/ProjectSummary.js
+++ reactfirebase/src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from "react"
+import moment from 'moment'
 
 const ProjectSummary = ({project}) =>{
     return(
@@ -7,7 +8,7 @@ const ProjectSummary = ({project}) =>{
  <span className="card-title">{project.title}</span>
  <p>{project.content}</p>
  <p>posted by {project.authorFirstName} {project.authorSecondName}</p>
-    <p className="grey-text">{project.createdAt.toDate().toString()}</p>
+    <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
             </div> 
     )
 }
